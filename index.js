af3b527ef25c354701209042a740fd4a82ad1de2
Vianney Lecroart <acemtp@gmail.com>
2016-01-25 09:07:34 +0100

    missing exports for legacyrtm and datastore

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
   WebClient: require('./lib/clients/web/client'),
   RtmClient: require('./lib/clients/rtm/client'),
+  LegacyRtmClient: require('./lib/clients/default/legacy-rtm'),
   EVENTS: require('./lib/clients/events'),
+  MemoryDataStore: require('./lib/data-store/memory-data-store'),
 };
