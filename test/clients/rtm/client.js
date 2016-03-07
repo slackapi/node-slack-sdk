bd4544890ee43d0ac3a862acac4a615af8102c1b
l12s <leah@l2s.com>
2016-03-06 17:11:39 -0800

    Adds tests for the updates to the `send` and `sendMessage` functions
    Adds:
    - tests for the _registerMsgHandler and _handleMsgResponse functions
    - tests to check that the RTMClient `send` and `sendMessage` functions correctly error when not connected.
    
    Also fixes a bad module.exports in the errors.js file

--- test/clients/rtm/client.js
+++ test/clients/rtm/client.js
@@ -116,4 +116,85 @@ describe('RTM API Client', function () {
 
   });
 
+  describe('Message Sending', function () {
+
+    it('should call a cb with an err when the RTM client is not connected', function (done) {
+      var rtm = new RtmAPIClient('fake-token');
+      rtm.sendMessage('test', 'test', function (err, res) {
+        expect(err).to.not.equal(null);
+        expect(res).to.equal(null);
+        done();
+      });
+    });
+
+    it('should call a catch cb with an err when the RTM client is not connected', function (done) {
+      var rtm = new RtmAPIClient('fake-token');
+      rtm.sendMessage('test', 'test')
+        .catch(function (err) {
+          expect(err).to.not.equal(null);
+          done();
+        });
+    });
+
+    it('should call the `ws.send` method when the send or sendMessage function is called');
+
+  });
+
+  describe('Message Response Handling', function () {
+
+    describe('#_registerMsgHandler()', function () {
+
+      it('should write to _msgResponseHandlers when _registerMsgHandler is called', function () {
+        var rtm = new RtmAPIClient('fake-token');
+        var fakeHandler = { fulfill: null, reject: null };
+        rtm._registerMsgHandler(1, fakeHandler);
+        expect(rtm._msgResponseHandlers[1]).to.deep.equal(fakeHandler);
+      });
+
+    });
+
+    describe('#_handleMsgResponse()', function () {
+
+      var setupRTMClient = function (handler) {
+        var rtm = new RtmAPIClient('fake-token');
+        rtm._registerMsgHandler(1, handler);
+
+        return rtm;
+      };
+
+      it('calls a registered callback fn when a message response or err is received', function () {
+        var handler = sinon.spy();
+        var rtm = setupRTMClient(handler);
+        rtm._handleMsgResponse(1, null, 'test');
+
+        expect(handler.calledWith(null, 'test')).to.equal(true);
+      });
+
+      it('should call a registered fulfill fn when a message response is received', function () {
+        var handler = { fulfill: sinon.spy(), reject: null };
+        var rtm = setupRTMClient(handler);
+        rtm._handleMsgResponse(1, null, 'test');
+
+        expect(handler.fulfill.calledWith('test')).to.equal(true);
+      });
+
+      it('should call a registered reject fn when a message response is received', function () {
+        var handler = { fulfill: null, reject: sinon.spy() };
+        var rtm = setupRTMClient(handler);
+        rtm._handleMsgResponse(1, 'test', null);
+
+        expect(handler.reject.calledWith('test')).to.equal(true);
+      });
+
+      it('deletes the response handler when a message response or err is received', function () {
+        var rtm = setupRTMClient(function () {});
+        rtm._handleMsgResponse(1, null, 'test');
+
+        expect(rtm._msgResponseHandlers[1]).to.equal(undefined);
+      });
+
+    });
+
+  });
+
 });
