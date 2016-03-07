9f39dd1456043a955bf1feaf2c82b1d5ede0ab43
anskaal <anskaal@gmail.com>
2016-03-07 08:41:26 +0100

    Changed rtm-client back to the way it was and improved example code.
    Client did already support reactions...

--- examples/example-rtm-client.js
+++ examples/example-rtm-client.js
@@ -5,20 +5,21 @@
 /* eslint no-console:0 */
 
 var RtmClient = require('../lib/clients/rtm/client');
+var RTM_EVENTS = require('../lib/clients/events/rtm').EVENTS;
 
 var token = process.env.SLACK_API_TOKEN || '';
 
 var rtm = new RtmClient(token, { logLevel: 'debug' });
 rtm.start();
 
-rtm.on('message', function handleRtmMessage(message) {
-  console.log(message);
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  console.log("Message:", message);
 });
 
-rtm.on('reaction_added', function handleRtmReactionAdded(reaction) {
-  console.log(reaction);
+rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
+  console.log("Reaction added:", reaction);
 });
 
-rtm.on('reaction_removed', function handleRtmReactionRemoved(reaction) {
-  console.log(reaction);
+rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
+  console.log("Reaction removed:", reaction);
 });
