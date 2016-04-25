b3d5f98cff5fb0f14ef9fcbf40494622b67f7a33
l12s <leah@l2s.com>
2016-04-25 01:19:58 -0700

    Cleans up some lint errors and adds an eslintrc file to the examples directory

--- examples/example-rtm-client-datastore.js
+++ examples/example-rtm-client-datastore.js
@@ -1,25 +1,27 @@
-var RtmClient       = require('@slack/client').RtmClient,
-    
-    CLIENT_EVENTS   = require('@slack/client').CLIENT_EVENTS,
-    RTM_EVENTS      = require('@slack/client').RTM_EVENTS,
-    
-    MemoryDataStore = require('@slack/client').MemoryDataStore;
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var MemoryDataStore = require('@slack/client').MemoryDataStore;
 
 var token = process.env.SLACK_API_TOKEN || '';
 
-var rtm = new RtmClient(token, { 
-    logLevel: 'info', // check this out for more on logger: https://github.com/winstonjs/winston
-    dataStore: new MemoryDataStore({}) // pass on a new instance of MemoryDataStore type to cache information
+var rtm = new RtmClient(token, {
+  logLevel: 'info', // check this out for more on logger: https://github.com/winstonjs/winston
+  dataStore: new MemoryDataStore({}) // pass a new MemoryDataStore instance to cache information
 });
 
 rtm.start();
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-    console.log('RTM client authenticated!');
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function handleRTMAuthenticated() {
+  console.log('RTM client authenticated!');
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('User %s posted a message in %s channel', rtm.dataStore.getUserById(message.user).name, rtm.dataStore.getChannelGroupOrDMById(message.channel).name);
+  console.log(
+    'User %s posted a message in %s channel',
+    rtm.dataStore.getUserById(message.user).name,
+    rtm.dataStore.getChannelGroupOrDMById(message.channel).name
+  );
 });
 
 rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
