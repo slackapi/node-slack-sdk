252ca6923a2e9b36c0f6ef6bfa4c92b80a677fbd
Don Goodman-Wilson <don@slack-corp.com>
2016-12-13 11:33:59 -0800

    passing tests and linter

--- test/helpers.js
+++ test/helpers.js
@@ -1,12 +1,31 @@
 var expect = require('chai').expect;
-
 var helpers = require('../lib/helpers');
 
 describe('Version strings', function () {
   describe('getVersionString', function () {
     it('returns the current version, when unmodified', function () {
       var str = helpers.getVersionString();
-      expect(str).to.equal('@slack/client/3.7.0'); //TODO the slash in the package name worries me
+      // TODO the slash in the package name worries me
+      expect(str).to.equal('@slack/client/3.7.0');
+    });
+  });
+
+  describe('appendToVersionString', function () {
+    it('appends a new value', function () {
+      var str;
+      helpers.appendToVersionString('foobar', '1.2');
+      str = helpers.getVersionString();
+      expect(str).to.equal('@slack/client/3.7.0 foobar/1.2');
+    });
+  });
+
+  describe('appendToVersionString', function () {
+    it('should never overwrite an existing value', function () {
+      var str;
+      helpers.appendToVersionString('@slack/client', '1.2');
+      str = helpers.getVersionString();
+      // TODO that the order of these tests _matters_ bothers me
+      expect(str).to.equal('@slack/client/3.7.0 foobar/1.2 @slack/client/1.2');
     });
   });
 });
