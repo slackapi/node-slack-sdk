fd8296ed2d00cff39a3d35c281294435c70dd802
Don Goodman-Wilson <don@slack-corp.com>
2016-12-13 11:00:25 -0800

    Get the actual package name and version

--- test/helpers.js
+++ test/helpers.js
@@ -6,7 +6,7 @@ describe('Version strings', function () {
   describe('getVersionString', function () {
     it('returns the current version, when unmodified', function () {
       var str = helpers.getVersionString();
-      expect(str).to.equal('foobar/1.2.3');
+      expect(str).to.equal('@slack/client/3.7.0'); //TODO the slash in the package name worries me
     });
   });
 });
