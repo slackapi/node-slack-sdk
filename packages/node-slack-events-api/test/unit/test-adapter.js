47b49446fad8a666f623fc473c1a70d82dc972a2
Ankur Oberoi <aoberoi@gmail.com>
2017-01-04 20:18:08 -0800

    downgrade syntax for test code to ES5

--- packages/node-slack-events-api/test/unit/test-adapter.js
+++ packages/node-slack-events-api/test/unit/test-adapter.js
@@ -1,59 +1,51 @@
-// import assert from 'assert';
-// import http from 'http';
-// import EventEmitter from 'events';
-//
-// import SlackEventAdapter from '../../src/adapter';
-
-const assert = require('assert');
-const http = require('http');
-const EventEmitter = require('events');
-const SlackEventAdapter = require('../../dist/adapter').default;
+var assert = require('assert');
+var http = require('http');
+var EventEmitter = require('events');
+var SlackEventAdapter = require('../../dist/adapter').default;
 
 // fixtures and test helpers
-const workingVerificationToken = 'VERIFICATION_TOKEN';
+var workingVerificationToken = 'VERIFICATION_TOKEN';
 function fakeBindMiddlewareToAdapter(adapter) {
   // eslint-disable-next-line no-param-reassign
   adapter.middleware = function _fakeMiddleware() { };
 }
 
-describe('SlackEventAdapter', () => {
-  describe('constructor', () => {
-    it('should be an EventEmitter subclass', () => {
-      const adapter = new SlackEventAdapter(workingVerificationToken);
+describe('SlackEventAdapter', function () {
+  describe('constructor', function () {
+    it('should be an EventEmitter subclass', function () {
+      var adapter = new SlackEventAdapter(workingVerificationToken);
       assert(adapter instanceof EventEmitter);
     });
-    it('should fail without a verification token', () => {
-      assert.throws(() => {
-        const adapter = new SlackEventAdapter();  // eslint-disable-line no-unused-vars
+    it('should fail without a verification token', function () {
+      assert.throws(function () {
+        var adapter = new SlackEventAdapter();  // eslint-disable-line no-unused-vars
       }, TypeError);
     });
-    it('should store the verification token', () => {
-      const adapter = new SlackEventAdapter(workingVerificationToken);
+    it('should store the verification token', function () {
+      var adapter = new SlackEventAdapter(workingVerificationToken);
       assert.equal(adapter.verificationToken, workingVerificationToken);
     });
   });
 
-  describe('#expressServer()', () => {
-    let adapter;
-    beforeEach(() => {
-      adapter = new SlackEventAdapter(workingVerificationToken);
-    });
-    afterEach(() => {
-      adapter = null;
+  describe('#expressServer()', function () {
+    beforeEach(function () {
+      this.adapter = new SlackEventAdapter(workingVerificationToken);
     });
 
-    it('should reject if the adapter does not have middleware bound', () => { // eslint-disable-line arrow-body-style
-      return adapter.expressServer()
-        .then((server) => {
+    it('should reject if the adapter does not have middleware bound', function () {
+      return this.adapter.expressServer()
+        .then(function (server) {
           assert.fail(server, null, 'a server was created');
-        }).catch((error) => {
+        }).catch(function (error) {
           assert(error instanceof Error);
         });
     });
 
-    it('should return a Promise of an http.Server', () => { // eslint-disable-line arrow-body-style
-      fakeBindMiddlewareToAdapter(adapter);
-      return adapter.expressServer().then(server => assert(server instanceof http.Server));
+    it('should return a Promise of an http.Server', function () {
+      fakeBindMiddlewareToAdapter(this.adapter);
+      return this.adapter.expressServer().then(function (server) {
+        assert(server instanceof http.Server);
+      });
     });
 
     it('should throw if the express or body-parser packages are not found');
