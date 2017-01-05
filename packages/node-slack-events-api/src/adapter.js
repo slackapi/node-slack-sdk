63c00f713be711d9c7d8183f319c21a26e53cde0
Ankur Oberoi <aoberoi@gmail.com>
2017-01-04 22:32:01 -0800

    rewrite error handling to resolve issues in tests

--- packages/node-slack-events-api/src/adapter.js
+++ packages/node-slack-events-api/src/adapter.js
@@ -2,6 +2,19 @@ import EventEmitter from 'events';
 import http from 'http';
 import isString from 'lodash.isstring';
 
+export const errorCodes = {
+  MIDDLEWARE_NOT_BOUND: 'SLACKEVENTADAPTER_MIDDLEWARE_NOT_BOUND',
+};
+
+// Private
+function checkMiddlewareBound(adapter) {
+  if (!adapter.middleware) {
+    const error = new Error('An express server can only be created after this adapter has been bound to middleware');
+    error.code = errorCodes.MIDDLEWARE_NOT_BOUND;
+    throw error;
+  }
+}
+
 export default class SlackEventAdapter extends EventEmitter {
   constructor(verificationToken) {
     if (!isString(verificationToken)) {
@@ -14,37 +27,33 @@ export default class SlackEventAdapter extends EventEmitter {
   // TODO: options (like https)
   expressServer() {
     return new Promise((resolve) => {
-      this.checkMiddlewareBound();
+      checkMiddlewareBound(this);
       resolve();
-    }).then(() => Promise.all([
-      System.import('express'),
-      System.import('body-parser'),
-      // System.import('express'),
-      // System.import('body-parser'),
+    })
+    .then(() => { // eslint-disable-line arrow-body-style
+      return Promise.all([
+        System.import('express'),
+        System.import('body-parser'),
+        // System.import('express'),
+        // System.import('body-parser'),
 
-      // The previous lines should be written as the comment following it, since `System.import()`
-      // is going to disappear after dynamic imports land (https://github.com/tc39/proposal-dynamic-import).
-      // There are two problems with using dynamic imports now:
-      //   1. eslint's parser (espree) doesn't support parsing it, and while babel's parser can,
-      //      using `babel-eslint` in order to delegate eslint's parsing to babel did not work.
-      //   2. there are no babel transforms for this syntax that seem to work at the moment. the
-      //      following was meant to work but ended up not working:
-      //      https://github.com/pwmckenna/babel-plugin-transform-import-commonjs.
-    ]).then(([express, bodyParser]) => {
+        // The previous lines should be written as the comment following it, since `System.import()`
+        // is going to disappear after dynamic imports land (https://github.com/tc39/proposal-dynamic-import).
+        // There are two problems with using dynamic imports now:
+        //   1. eslint's parser (espree) doesn't support parsing it, and while babel's parser can,
+        //      using `babel-eslint` in order to delegate eslint's parsing to babel did not work.
+        //   2. there are no babel transforms for this syntax that seem to work at the moment. the
+        //      following was meant to work but ended up not working:
+        //      https://github.com/pwmckenna/babel-plugin-transform-import-commonjs.
+      ]);
+    })
+    .then(([express, bodyParser]) => {
       const app = express();
       app.use(bodyParser.json());
       app.post('/event', this.middleware);
 
       return http.createServer(app);
-    }).catch((error) => {
-      // TODO: inspect errors and throw appropriately
-      throw error;
-    }));
+    });
   }
 
-  checkMiddlewareBound() {
-    if (!this.middleware) {
-      throw new Error('An express server can only be created after this adapter has been bound to middleware');
-    }
-  }
 }
--- packages/node-slack-events-api/test/unit/test-adapter.js
+++ packages/node-slack-events-api/test/unit/test-adapter.js
@@ -2,9 +2,9 @@ var assert = require('assert');
 var http = require('http');
 var EventEmitter = require('events');
 var proxyquire = require('proxyquire');
-var express = require('express');
-var bodyParser = require('body-parser');
-var SlackEventAdapter = require('../../dist/adapter').default;
+var systemUnderTest = require('../../dist/adapter');
+var errorCodes = systemUnderTest.errorCodes;
+var SlackEventAdapter = systemUnderTest.default;
 
 // fixtures and test helpers
 var workingVerificationToken = 'VERIFICATION_TOKEN';
@@ -39,43 +39,54 @@ describe('SlackEventAdapter', function () {
       return this.adapter.expressServer()
         .then(function (server) {
           assert.fail(server, null, 'a server was created');
-        }).catch(function (error) {
-          assert(error instanceof Error);
+        })
+        .catch(function (error) {
+          if (error.code === errorCodes.MIDDLEWARE_NOT_BOUND) {
+            assert(true);
+          } else {
+            throw error;
+          }
         });
     });
 
     describe('when express package is not found', function () {
       beforeEach(function () {
-        proxyquire('../../dist/adapter', { express: null });
-      });
-      afterEach(function () {
-        proxyquire('../../dist/adapter', { express: express });
+        SlackEventAdapter = proxyquire('../../dist/adapter', { express: null }).default;
+        this.adapter = new SlackEventAdapter(workingVerificationToken);
       });
       it('should reject', function () {
+        fakeBindMiddlewareToAdapter(this.adapter);
         return this.adapter.expressServer()
           .then(function (server) {
-            assert(server, null, 'a server was created');
+            assert.fail(server, null, 'a server was created');
           })
           .catch(function (error) {
-            assert(error instanceof Error);
+            if (error.code === 'MODULE_NOT_FOUND') {
+              assert(true);
+            } else {
+              throw error;
+            }
           });
       });
     });
 
     describe('when body-parser package is not found', function () {
       beforeEach(function () {
-        proxyquire('../../dist/adapter', { 'body-parser': null });
-      });
-      afterEach(function () {
-        proxyquire('../../dist/adapter', { 'body-parser': bodyParser });
+        SlackEventAdapter = proxyquire('../../dist/adapter', { 'body-parser': null }).default;
+        this.adapter = new SlackEventAdapter(workingVerificationToken);
       });
       it('should reject', function () {
+        fakeBindMiddlewareToAdapter(this.adapter);
         return this.adapter.expressServer()
           .then(function (server) {
-            assert(server, null, 'a server was created');
+            assert.fail(server, null, 'a server was created');
           })
           .catch(function (error) {
-            assert(error instanceof Error);
+            if (error.code === 'MODULE_NOT_FOUND') {
+              assert(true);
+            } else {
+              throw error;
+            }
           });
       });
     });
