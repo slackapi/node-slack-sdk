8854203e5d66974a419fc8167e720cf955ed0f05
Ankur Oberoi <aoberoi@gmail.com>
2017-01-06 11:32:44 -0800

    fix eslint parsing of dynamic imports, update reason it still can't be used

--- packages/node-slack-events-api/src/adapter.js
+++ packages/node-slack-events-api/src/adapter.js
@@ -34,17 +34,14 @@ export default class SlackEventAdapter extends EventEmitter {
       return Promise.all([
         System.import('express'),
         System.import('body-parser'),
-        // System.import('express'),
-        // System.import('body-parser'),
+        // import('express'),
+        // import('body-parser'),
 
         // The previous lines should be written as the comment following it, since `System.import()`
         // is going to disappear after dynamic imports land (https://github.com/tc39/proposal-dynamic-import).
-        // There are two problems with using dynamic imports now:
-        //   1. eslint's parser (espree) doesn't support parsing it, and while babel's parser can,
-        //      using `babel-eslint` in order to delegate eslint's parsing to babel did not work.
-        //   2. there are no babel transforms for this syntax that seem to work at the moment. the
-        //      following was meant to work but ended up not working:
-        //      https://github.com/pwmckenna/babel-plugin-transform-import-commonjs.
+        // There are no babel transforms for this syntax that seem to work at the moment. The
+        // following was meant to work but ended up not working:
+        // https://github.com/pwmckenna/babel-plugin-transform-import-commonjs.
       ]);
     })
     .then(([express, bodyParser]) => {
