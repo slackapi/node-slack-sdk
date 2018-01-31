41811d1ee951227130e861e548cece5558d0e47b
Ankur Oberoi <aoberoi@gmail.com>
2018-01-31 12:49:44 -0800

    update tslint and its configuration
    this commit passes `npm run lint:ts` and `npm run build`.

--- index.ts
+++ index.ts
@@ -1,19 +1,31 @@
-// tslint:disable:no-require-imports no-var-requires
-const events = require('./lib/clients/events');
-const retryPolicies = require('./lib/clients/retry-policies');
+import events = require('./lib/clients/events'); // tslint:disable-line:no-require-imports
+import RETRY_POLICIES = require('./lib/clients/retry-policies'); // tslint:disable-line:no-require-imports import-name
+import WebClient = require('./lib/clients/web/client'); // tslint:disable-line:no-require-imports import-name
+import RtmClient = require('./lib/clients/rtm/client'); // tslint:disable-line:no-require-imports import-name
+// tslint:disable-next-line:no-require-imports import-name
+import IncomingWebhook = require('./lib/clients/incoming-webhook/client');
+// tslint:disable-next-line:no-require-imports import-name
+import LegacyRtmClient = require('./lib/clients/default/legacy-rtm');
+// tslint:disable-next-line:no-require-imports import-name
+import MemoryDataStore = require('./lib/data-store/memory-data-store');
+import { requestOptionsTransport } from './lib/clients/transports/request';
 
-module.exports = {
-  WebClient: require('./lib/clients/web/client'),
-  RtmClient: require('./lib/clients/rtm/client'),
-  IncomingWebhook: require('./lib/clients/incoming-webhook/client'),
-  LegacyRtmClient: require('./lib/clients/default/legacy-rtm'),
-  MemoryDataStore: require('./lib/data-store/memory-data-store'),
-  requestOptionsTransport: require('./lib/clients/transports/request').requestOptionsTransport,
-  CLIENT_EVENTS: {
-    WEB: events.CLIENT_EVENTS.WEB,
-    RTM: events.CLIENT_EVENTS.RTM
-  },
-  RTM_EVENTS: events.RTM_EVENTS,
-  RTM_MESSAGE_SUBTYPES: events.RTM_MESSAGE_SUBTYPES,
-  RETRY_POLICIES: retryPolicies
+const CLIENT_EVENTS = {
+  WEB: events.CLIENT_EVENTS.WEB,
+  RTM: events.CLIENT_EVENTS.RTM,
+};
+
+const { RTM_EVENTS, RTM_MESSAGE_SUBTYPES } = events;
+
+export {
+  WebClient,
+  RtmClient,
+  IncomingWebhook,
+  LegacyRtmClient,
+  MemoryDataStore,
+  requestOptionsTransport,
+  CLIENT_EVENTS,
+  RTM_EVENTS,
+  RTM_MESSAGE_SUBTYPES,
+  RETRY_POLICIES,
 };
