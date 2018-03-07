cae00fa16d2d718720cef1f803fb96e4587e2379
Ankur Oberoi <aoberoi@gmail.com>
2018-03-07 08:29:08 -0800

    POC

---
+++ jsdoc/out/@slack-client-dist-errors.js
@@ -0,0 +1,11 @@
+/** 
+ * @module @slack/client/dist/errors
+ */
+
+/**
+ * Factory for producing a {@link CodedError} from a generic error
+ * @param {Error} error
+ * @param {module:@slack/client/dist/errors.ErrorCode} code
+ * @returns {module:@slack/client.CodedError}
+ */
+export function errorWithCode() {}
