189905039fc426074f55477529a76330553330de
Anton Kharitonov <anton.kharitonov@sap.com>
2018-03-15 16:09:22 +0100

    increase test timeout in order to avoid unexpected build failure

--- test/typescript/test.ts
+++ test/typescript/test.ts
@@ -4,7 +4,7 @@ import { check, checkDirectory } from 'typings-tester';
 describe('typescript typings tests', () => {
   it('should allow WebClient to work without a token', () => {
     check([`${__dirname}/sources/webclient-no-token.ts`], `${__dirname}/tsconfig-strict.json`);
-  });
+  }).timeout(10000);
 
   it('should export method argument types and enforce strictness in the right ways', () => {
     check([`${__dirname}/sources/method-argument-types.ts`], `${__dirname}/tsconfig-strict.json`);
