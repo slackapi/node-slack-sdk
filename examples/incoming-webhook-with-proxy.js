b6da7ad94bf6d5905f41192da31e893a90ba7e08
Ankur Oberoi <aoberoi@gmail.com>
2019-02-14 04:35:16 -0800

    modernize examples in example directory

--- examples/incoming-webhook-with-proxy.js
+++ examples/incoming-webhook-with-proxy.js
@@ -15,6 +15,7 @@ const proxyPort = process.env.SLACK_PROXY_PORT || '';
 const proxyScheme = process.env.SLACK_PROXY_SCHEME || 'http';
 const proxyEndpoint = `${proxyScheme}://${proxyUrl}:${proxyPort}`;
 
+// Initialize a custom agent for an HTTP proxy
 const tunnel = require('tunnel');
 const tunnelAgent = tunnel.httpsOverHttp({
   proxy: {
@@ -22,23 +23,23 @@ const tunnelAgent = tunnel.httpsOverHttp({
     port: proxyPort
   }
 });
+
+// Initialize the IncomingWebhook using the custom agent
 const webhookTunnelAgent = new IncomingWebhook(url, { agent: tunnelAgent } );
 
-//Send IncomingWebhook Message over tunnel
+// Send IncomingWebhook Message over tunnel
 sendWebhookMessage(webhookTunnelAgent, 'Hello World, over tunnel agent!');
 
+// Initialize a custom agent again, but this time using a different package
 const HttpsProxyAgent = require('https-proxy-agent');
 const httpsProxyAgent = new HttpsProxyAgent(proxyEndpoint);
+
+// Initialize the IncomingWebhook using the new custom agent
 const webhookHttpsProxyAgent = new IncomingWebhook(url, { agent: httpsProxyAgent });
 
-//Send IncomingWebhook Message over Proxy Agent
+// Send IncomingWebhook Message over Proxy Agent
 sendWebhookMessage(webhookHttpsProxyAgent, 'Hello World, over https-proxy-agent!');
 
-/**
- *
- * @param webhookInstance
- * @param message
- */
 function sendWebhookMessage(webhookInstance, message) {
   webhookInstance.send({
     text: message
