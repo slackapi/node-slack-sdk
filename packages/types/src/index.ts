d0461f4b51be6f468c3677f15120e34b8fabefe5
David DeRemer <david@verygood.ventures>
2019-06-05 20:55:42 -0400

    Update index.ts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -127,7 +127,7 @@ export interface Button extends Action {
   text: PlainTextElement;
   value?: string;
   url?: string;
-  style?: 'danger' | 'default' | 'primary';
+  style?: 'danger' | 'primary';
 }
 
 export interface Overflow extends Action {
