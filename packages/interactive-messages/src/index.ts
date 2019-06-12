7b6ce9902eb0ccd56ffba95a09e2e3aecff8fd10
Calvin Watford <cwatford@slack-corp.com>
2019-06-12 15:35:57 -0700

    Fix linting `@slack/interactive-messages`

--- packages/interactive-messages/src/index.ts
+++ packages/interactive-messages/src/index.ts
@@ -1,25 +1,15 @@
-/**
- * @module @slack/interactive-messages
- */
-
 import { SlackMessageAdapter, MessageAdapterOptions } from './adapter';
 import { ErrorCode } from './errors';
 
 /**
  * Dictionary of error codes that may appear on errors emitted from this package's objects
- * @readonly
- * @enum {string}
  */
 export const errorCodes = {
   BODY_PARSER_NOT_PERMITTED: ErrorCode.BodyParserNotPermitted,
-};
+} as const;
 
 /**
- * Factory method to create an instance of {@link module:adapter~SlackMessageAdapter}
- *
- * @param {string} signingSecret
- * @param {Object} options
- * @returns {module:adapter~SlackMessageAdapter}
+ * Factory method to create an instance of {@link SlackMessageAdapter}
  */
 export function createMessageAdapter(signingSecret: string, options?: MessageAdapterOptions): SlackMessageAdapter {
   return new SlackMessageAdapter(signingSecret, options);
