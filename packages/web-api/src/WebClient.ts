70aaac2e005a28f2e2104d9ed9d7c5241a6044e0
Suyash Gulati <suyashgulati@gmail.com>
2020-04-28 04:26:24 +0530

    Lint fix

--- packages/web-api/src/WebClient.ts
+++ packages/web-api/src/WebClient.ts
@@ -471,7 +471,8 @@ export class WebClient extends EventEmitter<WebClientEvent> {
     open: (this.apiCall.bind(this, 'views.open')) as Method<methods.ViewsOpenArguments>,
     publish: (this.apiCall.bind(this, 'views.publish')) as Method<methods.ViewsPublishArguments>,
     push: (this.apiCall.bind(this, 'views.push')) as Method<methods.ViewsPushArguments>,
-    update: (this.apiCall.bind(this, 'views.update')) as Method<methods.ViewsUpdateArgumentsWithViewId | methods.ViewsUpdateArgumentsWithExternalId>,
+    update: (this.apiCall.bind(this, 'views.update')) as
+      Method<methods.ViewsUpdateArgumentsWithViewId | methods.ViewsUpdateArgumentsWithExternalId>,
   };
 
   /**
