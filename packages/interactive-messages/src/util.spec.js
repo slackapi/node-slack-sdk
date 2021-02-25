0456eb09985832cec5cbf10f150dced9a10fa92c
Oleksandr Shchyhol <oshchyhol@users.noreply.github.com>
2021-02-26 00:09:13 +0100

    [#1180] Clear timer when input promise resolves before timeout (PR #1181)

--- packages/interactive-messages/src/util.spec.js
+++ packages/interactive-messages/src/util.spec.js
@@ -2,6 +2,7 @@ require('mocha');
 const { assert } = require('chai');
 const { promiseTimeout, errorCodes } = require('./util');
 const { delayed } = require('../test/helpers');
+const sinon = require('sinon');
 
 // test suite
 describe('promiseTimeout', function () {
@@ -42,4 +43,50 @@ describe('promiseTimeout', function () {
       assert.equal(error.code, errorCodes.PROMISE_TIMEOUT);
     });
   });
+
+  describe('timers', function() {
+    beforeEach(function() {
+      this.clock = sinon.useFakeTimers()
+    })
+
+    afterEach(function() {
+      this.clock.restore()
+    });
+
+    it('should clear timer when input resolves faster than timeout', function () {
+      sinon.spy(this.clock, 'clearTimeout');
+      const value = 'test';
+      const input = new Promise(function (resolve) {
+        setTimeout(function() {
+          resolve(value);
+        }, 10);
+      });
+      const output = promiseTimeout(20, input);
+      this.clock.tick(15);
+      const self = this;
+      return output.then(function (v) {
+        assert.isTrue(self.clock.clearTimeout.calledOnce);
+        assert.equal(v, value);
+      });
+    });
+
+    it('should clear timer when input resolves slower than timeout', function () {
+      sinon.spy(this.clock, 'clearTimeout');
+      const value = 'test';
+      const input = new Promise(function (resolve) {
+        setTimeout(function() {
+          resolve(value);
+        }, 20);
+      });
+      const output = promiseTimeout(10, input);
+      this.clock.tick(11);
+      const self = this;
+      return output.then(function (value) {
+        throw new Error('should not resolve. value: ' + value);
+      }, function (error) {
+        assert.isTrue(self.clock.clearTimeout.calledOnce);
+        assert.equal(error.code, errorCodes.PROMISE_TIMEOUT);
+      });
+    });
+  })
 });
