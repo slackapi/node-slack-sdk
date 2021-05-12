43b9c41054e842f554e08f1e19681167580bc470
Kazuhiro Sera <seratch@gmail.com>
2021-05-12 19:00:16 +0900

    Fix #1235 ViewsOpenResponse type is incorrect

--- packages/web-api/src/response/UsersGetPresenceResponse.ts
+++ packages/web-api/src/response/UsersGetPresenceResponse.ts
@@ -1,9 +1,15 @@
 /* tslint:disable */
 import { WebAPICallResult } from '../WebClient';
 export type UsersGetPresenceResponse = WebAPICallResult & {
-  ok?:       boolean;
-  presence?: string;
-  error?:    string;
-  needed?:   string;
-  provided?: string;
+  ok?:               boolean;
+  warning?:          string;
+  error?:            string;
+  needed?:           string;
+  provided?:         string;
+  presence?:         string;
+  online?:           boolean;
+  auto_away?:        boolean;
+  manual_away?:      boolean;
+  connection_count?: number;
+  last_activity?:    number;
 };
--- packages/web-api/src/response/UsersIdentityResponse.ts
+++ packages/web-api/src/response/UsersIdentityResponse.ts
@@ -2,7 +2,27 @@
 import { WebAPICallResult } from '../WebClient';
 export type UsersIdentityResponse = WebAPICallResult & {
   ok?:       boolean;
+  warning?:  string;
   error?:    string;
   needed?:   string;
   provided?: string;
+  user?:     User;
+  team?:     Team;
 };
+
+export interface Team {
+  name?: string;
+  id?:   string;
+}
+
+export interface User {
+  name?:      string;
+  id?:        string;
+  email?:     string;
+  image_24?:  string;
+  image_32?:  string;
+  image_48?:  string;
+  image_72?:  string;
+  image_192?: string;
+  image_512?: string;
+}
