782f4a4c96fb9bc4dd6f1b98d5eec9800d93a710
Kazuhiro Sera <ksera@slack-corp.com>
2021-05-19 17:57:29 +0900

    Fix #1235 ViewsOpenResponse type is incorrect (#1236)
    * Fix #1235 ViewsOpenResponse type is incorrect
    
    * Update the types as of May 12, 2021

--- packages/web-api/src/response/DialogOpenResponse.ts
+++ packages/web-api/src/response/DialogOpenResponse.ts
@@ -1,8 +1,14 @@
 /* tslint:disable */
 import { WebAPICallResult } from '../WebClient';
 export type DialogOpenResponse = WebAPICallResult & {
-  ok?:       boolean;
-  error?:    string;
-  needed?:   string;
-  provided?: string;
+  ok?:                boolean;
+  warning?:           string;
+  error?:             string;
+  needed?:            string;
+  provided?:          string;
+  response_metadata?: ResponseMetadata;
 };
+
+export interface ResponseMetadata {
+  messages?: string[];
+}
