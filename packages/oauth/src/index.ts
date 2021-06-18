fbd160376943aa3975cbea8b742adb37e74259c8
Alissa Renz <alissa.renz@gmail.com>
2021-06-18 13:40:22 -0700

    Add support for FileInstallationStore

--- packages/oauth/src/index.ts
+++ packages/oauth/src/index.ts
@@ -11,6 +11,7 @@ import {
 } from './errors';
 import { parse as parseUrl, URLSearchParams, URL } from 'url';
 import { Logger, LogLevel, getLogger } from './logger';
+import { MemoryInstallationStore } from './stores';
 
 /**
  * InstallProvider Class.
@@ -455,93 +456,9 @@ export interface InstallationStore {
     installation: Installation<AuthVersion, boolean>,
     logger?: Logger): Promise<void>;
   fetchInstallation:
-    (query: InstallationQuery<boolean>, logger?: Logger) => Promise<Installation<'v1' | 'v2', boolean>>;
+  (query: InstallationQuery<boolean>, logger?: Logger) => Promise<Installation<'v1' | 'v2', boolean>>;
   deleteInstallation:
-    (query: InstallationQuery<boolean>, logger?: Logger) => Promise<void>;
-}
-
-// using a javascript object as a makeshift database for development
-// storing user tokens is not supported
-interface DevDatabase {
-  [teamIdOrEnterpriseId: string]: Installation;
-}
-
-// Default Install Store. Should only be used for development
-class MemoryInstallationStore implements InstallationStore {
-  public devDB: DevDatabase = {};
-
-  public async storeInstallation(installation: Installation, logger?: Logger): Promise<void> {
-    // NOTE: installations on a single workspace that happen to be within an enterprise organization are stored by
-    // the team ID as the key
-    // TODO: what about installations on an enterprise (acting as a single workspace) with `admin` scope, which is not
-    // an org install?
-    if (logger !== undefined) {
-      logger.warn('Storing Access Token. Please use a real Installation Store for production!');
-    }
-
-    if (isOrgInstall(installation)) {
-      if (logger !== undefined) {
-        logger.debug('storing org installation');
-      }
-      this.devDB[installation.enterprise.id] = installation;
-    } else if (isNotOrgInstall(installation)) {
-      if (logger !== undefined) {
-        logger.debug('storing single team installation');
-      }
-      this.devDB[installation.team.id] = installation;
-    } else {
-      throw new Error('Failed saving installation data to installationStore');
-    }
-  }
-
-  public async fetchInstallation(
-    query: InstallationQuery<boolean>,
-    logger?: Logger): Promise<Installation<'v1' | 'v2'>> {
-    if (logger !== undefined) {
-      logger.warn('Retrieving Access Token from DB. Please use a real Installation Store for production!');
-    }
-    if (query.isEnterpriseInstall) {
-      if (query.enterpriseId !== undefined) {
-        if (logger !== undefined) {
-          logger.debug('fetching org installation');
-        }
-        return this.devDB[query.enterpriseId] as OrgInstallation;
-      }
-    }
-    if (query.teamId !== undefined) {
-      if (logger !== undefined) {
-        logger.debug('fetching single team installation');
-      }
-      return this.devDB[query.teamId] as Installation<'v1' | 'v2', false>;
-    }
-    throw new Error('Failed fetching installation');
-  }
-
-  public async deleteInstallation(query: InstallationQuery<boolean>, logger?: Logger): Promise<void> {
-    if (logger !== undefined) {
-      logger.warn('Deleting Access Token from DB. Please use a real Installation Store for production!');
-    }
-
-    if (query.isEnterpriseInstall && query.enterpriseId !== undefined) {
-      if (logger !== undefined) {
-        logger.debug('deleting org installation');
-      }
-
-      const { [query.enterpriseId]: _, ...devDB } = this.devDB;
-      this.devDB = devDB;
-
-    } else if (query.teamId !== undefined) {
-      if (logger !== undefined) {
-        logger.debug('deleting single team installation');
-      }
-
-      const { [query.teamId]: _, ...devDB } = this.devDB;
-      this.devDB = devDB;
-
-    } else {
-      throw new Error('Failed to delete installation');
-    }
-  }
+  (query: InstallationQuery<boolean>, logger?: Logger) => Promise<void>;
 }
 
 /**
@@ -782,3 +699,4 @@ interface AuthTestResult extends WebAPICallResult {
 }
 
 export { Logger, LogLevel } from './logger';
+export { MemoryInstallationStore, FileInstallationStore } from './stores';
---
+++ packages/oauth/src/stores/file-store.ts
@@ -0,0 +1,116 @@
+import { Installation, InstallationStore, InstallationQuery } from '../index';
+import { Logger } from '../logger';
+import fs from 'fs';
+import path from 'path';
+import { homedir } from 'os';
+
+export interface FileInstallationOptions {
+  baseDir?: string;
+  historicalDataEnabled?: boolean;
+  clientId?: string;
+}
+
+export class FileInstallationStore implements InstallationStore {
+
+  private baseDir: string;
+  private historicalDataEnabled: boolean;
+
+  constructor({
+    baseDir = `${homedir()}/.bolt-app-installation`,
+    clientId,
+    historicalDataEnabled = true,
+  }: FileInstallationOptions = {}) {
+    this.baseDir = clientId !== undefined ? `${baseDir}/${clientId}` : baseDir;
+    this.historicalDataEnabled = historicalDataEnabled;
+  }
+
+  public async storeInstallation(installation: Installation, logger?: Logger): Promise<void> {
+    const { enterprise, team, user } = installation;
+    const installationData = JSON.stringify(installation);
+    const installationDir = this.getInstallationDir(enterprise?.id, team?.id);
+
+    if (logger !== undefined) { logger.warn('Storing installation in FileInstallationStore'); }
+
+    // Create Installation Directory
+    fs.mkdir(installationDir, { recursive: true }, (err) => {
+      if (err !== null) { return console.error(err); }
+    });
+
+    try {
+      writeToFile(`${installationDir}/app-latest`, installationData);
+      writeToFile(`${installationDir}/user-${user.id}-latest`, installationData);
+
+      if (this.historicalDataEnabled) {
+        const currentUTC = Date.now();
+        writeToFile(`${installationDir}/app-${currentUTC}`, installationData);
+        writeToFile(`${installationDir}/user-${user.id}-${currentUTC}`, installationData);
+      }
+    } catch (err) {
+      throw new Error('Failed to save installation to FileInstallationStore');
+    }
+  }
+
+  public async fetchInstallation(query: InstallationQuery<boolean>, logger?: Logger): Promise<Installation> {
+    const { enterpriseId, teamId } = query;
+    const installationDir = this.getInstallationDir(enterpriseId, teamId);
+
+    if (logger !== undefined) { logger.warn('Retrieving installation from FileInstallationStore'); }
+
+    try {
+      const data = fs.readFileSync(path.resolve(`${installationDir}/app-latest`));
+      const installation: Installation = JSON.parse(data.toString());
+      return installation;
+    } catch (err) {
+      throw new Error('Failed to fetch data from FileInstallationStore');
+    }
+  }
+
+  public async deleteInstallation(query: InstallationQuery<boolean>, logger?: Logger): Promise<void> {
+    const { enterpriseId, teamId, userId } = query;
+    const installationDir = this.getInstallationDir(enterpriseId, teamId);
+
+    if (logger !== undefined) { logger.warn('Deleting installation from FileInstallationStore'); }
+
+    let filesToDelete: string[] = ['app-latest'];
+
+    const appFiles = fs.readdirSync(installationDir).filter(file => file.includes('app-'));
+    filesToDelete = filesToDelete.concat(appFiles);
+
+    if (userId !== undefined) {
+      const userFiles = fs.readdirSync(installationDir).filter(file => file.includes(`user-${userId}-`));
+      filesToDelete = filesToDelete.concat(userFiles);
+    }
+
+    try {
+      filesToDelete.map(filePath => deleteFile(path.resolve(`${installationDir}/${filePath}`)));
+    } catch (err) {
+      throw new Error('Failed to delete installation from FileInstallationStore');
+    }
+  }
+
+  private getInstallationDir(enterpriseId = '', teamId = ''): string {
+    let installDir = `${this.baseDir}/${enterpriseId}`;
+
+    if (teamId !== '') {
+      installDir += (enterpriseId !== '') ? `-${teamId}` : `${teamId}`;
+    }
+
+    return installDir;
+  }
+}
+
+function writeToFile(filePath: string, data: string): void {
+  fs.writeFile(filePath, data, (err) => {
+    if (err !== null) {
+      throw new Error('There was an error writing to the InstallationStore');
+    }
+  });
+}
+
+function deleteFile(filePath: string): void {
+  fs.unlink(path.resolve(filePath), (err) => {
+    if (err !== null) {
+      throw new Error('Failed to delete installation from FileInstallationStore');
+    }
+  });
+}
