8d2cdd708c9a02384b09c2485db821b4f234ebd8
Sam Chung <samc@seek.com.au>
2021-06-19 22:13:16 +1000

    feat: add `SlashCommand` type

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -401,3 +401,22 @@ export interface CallUserExternal {
   display_name: string;
   avatar_url: string;
 }
+
+export interface SlashCommand extends Record<string, any> {
+  token: string;
+  command: string;
+  text: string;
+  response_url: string;
+  trigger_id: string;
+  user_id: string;
+  user_name: string;
+  team_id: string;
+  team_domain: string;
+  channel_id: string;
+  channel_name: string;
+  api_app_id: string;
+  enterprise_id?: string;
+  enterprise_name?: string;
+  // exists for enterprise installs
+  is_enterprise_install?: string; // This should be a boolean, but payload for commands gives string 'true'
+}
