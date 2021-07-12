aae422c526105235de37710224a999d058c4c51f
Richard Robinson <richard@rr.codes>
2021-07-12 17:58:11 -0400

    Fixed `PinsAddArguments` and `PinsRemoveArguments` interface (#1277)
    * Fixed `PinsAddArguments` interface
    
    * also updated `PinsRemoveArguments`

--- packages/web-api/src/methods.ts
+++ packages/web-api/src/methods.ts
@@ -1604,20 +1604,14 @@ export interface OAuthV2AccessArguments extends WebAPICallOptions {
  */
 export interface PinsAddArguments extends WebAPICallOptions, TokenOverridable {
   channel: string;
-  // must supply one of:
-  file?: string; // file id
-  file_comment?: string;
-  timestamp?: string;
+  timestamp: string;
 }
 export interface PinsListArguments extends WebAPICallOptions, TokenOverridable {
   channel: string;
 }
 export interface PinsRemoveArguments extends WebAPICallOptions, TokenOverridable {
   channel: string;
-  // must supply one of:
-  file?: string; // file id
-  file_comment?: string;
-  timestamp?: string;
+  timestamp: string;
 }
 
 /*
