e5efc171703e6acf90b3384dfba9fe669b69da90
Kazuhiro Sera <seratch@gmail.com>
2021-08-20 10:30:27 +0900

    Generate the latest web-api response types

--- packages/web-api/src/response/UsersLookupByEmailResponse.ts
+++ packages/web-api/src/response/UsersLookupByEmailResponse.ts
@@ -9,26 +9,27 @@ export type UsersLookupByEmailResponse = WebAPICallResult & {
 };
 
 export interface User {
-  id?:                  string;
-  team_id?:             string;
-  name?:                string;
-  deleted?:             boolean;
-  color?:               string;
-  real_name?:           string;
-  tz?:                  string;
-  tz_label?:            string;
-  tz_offset?:           number;
-  profile?:             Profile;
-  is_admin?:            boolean;
-  is_owner?:            boolean;
-  is_primary_owner?:    boolean;
-  is_restricted?:       boolean;
-  is_ultra_restricted?: boolean;
-  is_bot?:              boolean;
-  is_app_user?:         boolean;
-  updated?:             number;
-  has_2fa?:             boolean;
-  is_email_confirmed?:  boolean;
+  id?:                         string;
+  team_id?:                    string;
+  name?:                       string;
+  deleted?:                    boolean;
+  color?:                      string;
+  real_name?:                  string;
+  tz?:                         string;
+  tz_label?:                   string;
+  tz_offset?:                  number;
+  profile?:                    Profile;
+  is_admin?:                   boolean;
+  is_owner?:                   boolean;
+  is_primary_owner?:           boolean;
+  is_restricted?:              boolean;
+  is_ultra_restricted?:        boolean;
+  is_bot?:                     boolean;
+  is_app_user?:                boolean;
+  updated?:                    number;
+  has_2fa?:                    boolean;
+  is_email_confirmed?:         boolean;
+  who_can_share_contact_card?: string;
 }
 
 export interface Profile {
