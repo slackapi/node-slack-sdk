db952a5cd56ff5982cd5bb9da2710d2dd74743ff
Kazuhiro Sera <ksera@slack-corp.com>
2021-08-27 14:37:36 +0900

    Fix #1318 Add notice comments to auto-generated source files (#1320)
    * Fix #1318 Add notice comments to auto-generated source files
    * Update scripts/code_generator.rb
    * Renegerate files
    
    Co-authored-by: Michael Brooks <michael@michaelbrooks.ca>

--- scripts/generate-web-api-types.sh
+++ scripts/generate-web-api-types.sh
@@ -6,10 +6,15 @@ tmp_dir=${script_dir}/../tmp
 rm -rf ${tmp_dir}
 mkdir ${tmp_dir}
 cd ${tmp_dir}
+
+# Use the latest revision of the java-slack-sdk git repo
 git clone https://github.com/slackapi/java-slack-sdk.git
 
 cd -
 cd ${script_dir}
 
+# Download quicktype for generating code
 npm i
+
+# This Ruby script runs quicktype internally and do some additional modification to the generated files
 ruby ./code_generator.rb
