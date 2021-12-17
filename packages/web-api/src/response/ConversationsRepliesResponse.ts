df622ceddd673cc0efe2c0b1f19c325fe1bc7641
Kazuhiro Sera <ksera@slack-corp.com>
2021-12-17 12:13:21 +0900

    Update the web-api response types with the latest data source (#1405)

--- packages/web-api/src/response/ConversationsRepliesResponse.ts
+++ packages/web-api/src/response/ConversationsRepliesResponse.ts
@@ -37,6 +37,10 @@ export interface Message {
   is_locked?:         boolean;
   blocks?:            Block[];
   attachments?:       Attachment[];
+  last_read?:         string;
+  files?:             File[];
+  upload?:            boolean;
+  display_as_bot?:    boolean;
 }
 
 export interface Attachment {
@@ -239,6 +243,132 @@ export interface Icons {
   image_36?: string;
   image_48?: string;
   image_72?: string;
+}
+
+export interface File {
+  id?:                        string;
+  created?:                   number;
+  timestamp?:                 number;
+  name?:                      string;
+  title?:                     string;
+  subject?:                   string;
+  mimetype?:                  string;
+  filetype?:                  string;
+  pretty_type?:               string;
+  user?:                      string;
+  mode?:                      string;
+  editable?:                  boolean;
+  non_owner_editable?:        boolean;
+  editor?:                    string;
+  last_editor?:               string;
+  updated?:                   number;
+  original_attachment_count?: number;
+  is_external?:               boolean;
+  external_type?:             string;
+  external_id?:               string;
+  external_url?:              string;
+  username?:                  string;
+  size?:                      number;
+  url_private?:               string;
+  url_private_download?:      string;
+  app_id?:                    string;
+  app_name?:                  string;
+  thumb_64?:                  string;
+  thumb_64_gif?:              string;
+  thumb_64_w?:                string;
+  thumb_64_h?:                string;
+  thumb_80?:                  string;
+  thumb_80_gif?:              string;
+  thumb_80_w?:                string;
+  thumb_80_h?:                string;
+  thumb_160?:                 string;
+  thumb_160_gif?:             string;
+  thumb_160_w?:               string;
+  thumb_160_h?:               string;
+  thumb_360?:                 string;
+  thumb_360_gif?:             string;
+  thumb_360_w?:               string;
+  thumb_360_h?:               string;
+  thumb_480?:                 string;
+  thumb_480_gif?:             string;
+  thumb_480_w?:               string;
+  thumb_480_h?:               string;
+  thumb_720?:                 string;
+  thumb_720_gif?:             string;
+  thumb_720_w?:               string;
+  thumb_720_h?:               string;
+  thumb_800?:                 string;
+  thumb_800_gif?:             string;
+  thumb_800_w?:               string;
+  thumb_800_h?:               string;
+  thumb_960?:                 string;
+  thumb_960_gif?:             string;
+  thumb_960_w?:               string;
+  thumb_960_h?:               string;
+  thumb_1024?:                string;
+  thumb_1024_gif?:            string;
+  thumb_1024_w?:              string;
+  thumb_1024_h?:              string;
+  thumb_video?:               string;
+  thumb_gif?:                 string;
+  thumb_pdf?:                 string;
+  thumb_pdf_w?:               string;
+  thumb_pdf_h?:               string;
+  thumb_tiny?:                string;
+  converted_pdf?:             string;
+  image_exif_rotation?:       number;
+  original_w?:                string;
+  original_h?:                string;
+  deanimate?:                 string;
+  deanimate_gif?:             string;
+  pjpeg?:                     string;
+  permalink?:                 string;
+  permalink_public?:          string;
+  edit_link?:                 string;
+  has_rich_preview?:          boolean;
+  media_display_type?:        string;
+  preview_is_truncated?:      boolean;
+  preview?:                   string;
+  preview_highlight?:         string;
+  plain_text?:                string;
+  preview_plain_text?:        string;
+  has_more?:                  boolean;
+  sent_to_self?:              boolean;
+  lines?:                     number;
+  lines_more?:                number;
+  is_public?:                 boolean;
+  public_url_shared?:         boolean;
+  display_as_bot?:            boolean;
+  shares?:                    Shares;
+  channel_actions_ts?:        string;
+  channel_actions_count?:     number;
+  headers?:                   Headers;
+  simplified_html?:           string;
+  bot_id?:                    string;
+  initial_comment?:           InitialComment;
+  num_stars?:                 number;
+  is_starred?:                boolean;
+  comments_count?:            number;
+}
+
+export interface Headers {
+  date?:        string;
+  in_reply_to?: string;
+  reply_to?:    string;
+  message_id?:  string;
+}
+
+export interface InitialComment {
+  id?:        string;
+  created?:   number;
+  timestamp?: number;
+  user?:      string;
+  comment?:   string;
+  channel?:   string;
+  is_intro?:  boolean;
+}
+
+export interface Shares {
 }
 
 export interface ResponseMetadata {
