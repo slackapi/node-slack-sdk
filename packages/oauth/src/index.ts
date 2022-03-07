f134cf220c5cee462dc3fc1558d65dc5f2c9cbfc
Kazuhiro Sera <seratch@gmail.com>
2022-03-08 07:12:38 +0900

    Add more exports to oauth/index.ts (#1446)

--- packages/oauth/src/index.ts
+++ packages/oauth/src/index.ts
@@ -1,15 +1,33 @@
-export * from './errors';
 export { Logger, LogLevel } from './logger';
+
+// The inputs / outputs of the InstallProvider module
 export { AuthorizeResult } from './authorize-result';
-export { CallbackOptions } from './callback-options';
+export { Installation, OrgInstallation } from './installation';
+export { InstallationQuery, OrgInstallationQuery } from './installation-query';
+
+// The errors that can be returned by this module
+export * from './errors';
+
+// The core part of this library
 export {
   InstallProvider,
   OAuthV2TokenRefreshResponse,
   OAuthV2Response,
 } from './install-provider';
-export { Installation, OrgInstallation } from './installation';
-export { InstallationQuery, OrgInstallationQuery } from './installation-query';
+
 export { InstallProviderOptions } from './install-provider-options';
 export { InstallURLOptions } from './install-url-options';
+// the callback handlers for the `/slack/install` path
+export { InstallPathOptions } from './install-path-options';
+export { default as defaultRenderHtmlForInstallPath } from './default-render-html-for-install-path';
+// the callback handlers for the `/slack/oauth_redirect` path
+export {
+  CallbackOptions,
+  defaultCallbackFailure,
+  defaultCallbackSuccess,
+} from './callback-options';
+
+// InstallationStore interface and built-in implementations
 export * from './installation-stores';
+// StateStore interface and built-in implementations
 export * from './state-stores';
