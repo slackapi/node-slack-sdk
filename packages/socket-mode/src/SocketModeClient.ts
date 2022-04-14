553e7f4208c3451e7c80fe6edd56654147343128
Kazuhiro Sera <seratch@gmail.com>
2022-04-15 08:23:29 +0900

    Add envelop_id to Socket Mode listener arguments (#1466)

--- packages/socket-mode/src/SocketModeClient.ts
+++ packages/socket-mode/src/SocketModeClient.ts
@@ -594,6 +594,7 @@ export class SocketModeClient extends EventEmitter {
     if (event.type === 'events_api') {
       this.emit(event.payload.event.type, {
         ack,
+        envelope_id: event.envelope_id,
         body: event.payload,
         event: event.payload.event,
         retry_num: event.retry_attempt,
@@ -604,6 +605,7 @@ export class SocketModeClient extends EventEmitter {
       // emit just ack and body for all other types of messages
       this.emit(event.type, {
         ack,
+        envelope_id: event.envelope_id,
         body: event.payload,
         accepts_response_payload: event.accepts_response_payload,
       });
@@ -613,6 +615,7 @@ export class SocketModeClient extends EventEmitter {
     // used in tools like bolt-js
     this.emit('slack_event', {
       ack,
+      envelope_id: event.envelope_id,
       type: event.type,
       body: event.payload,
       retry_num: event.retry_attempt,
