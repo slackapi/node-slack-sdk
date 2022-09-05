688bb623b4c67a4a8b9480293fd85bc9d53a32ee
edwmurph <20996513+edwmurph@users.noreply.github.com>
2022-09-05 07:26:55 -0400

    fix: tweak webclient fallback logic + add test coverage

--- packages/web-api/src/WebClient.spec.js
+++ packages/web-api/src/WebClient.spec.js
@@ -229,6 +229,32 @@ describe('WebClient', function () {
       ];
       const warningTestPatterns = textWarningTestPatterns.concat(attachmentWarningTestPatterns);
 
+      attachmentWarningTestPatterns.reduce((acc, { method, args }) => {
+        const attachmentPatterns = [{ fallback: "fallback" }].map((attachmentOverrides) => {
+          const attachments = args.attachments.map( attachment => {
+            return Object.assign({}, attachment, attachmentOverrides);
+          })
+          return { method, args: Object.assign({}, args, { attachments }) };
+        });
+
+        return acc.concat(attachmentPatterns)
+      }, []).forEach(({ method, args }) => {
+        it(`should not send warning to logs when client executes ${method} without text but with attachment fallback argument`, function () {
+          const logger = {
+            debug: sinon.spy(),
+            info: sinon.spy(),
+            warn: sinon.spy(),
+            error: sinon.spy(),
+            setLevel: sinon.spy(),
+            setName: sinon.spy(),
+          };
+          const warnClient = new WebClient(token, { logLevel: LogLevel.WARN, logger });
+          return warnClient.apiCall(method, args).then(() => {
+            assert.equal(logger.warn.callCount, 3)
+          });
+        });
+      });
+
       warningTestPatterns.reduce((acc, { method, args }) => {
         const textPatterns = [{ text: "text" }]
           .map((v) => ({ method, args: Object.assign({}, v, args) }))
@@ -278,7 +304,7 @@ describe('WebClient', function () {
           .map((v) => ({ method, args: Object.assign({}, v, args) }))
         return acc.concat(textPatterns)
       }, []).forEach(({ method, args }) => {
-        it(`should send attachment fallback warning to logs when client executes ${method} without text argument(${args.text === "" ? "empty" : args.text}) nor without attachment-level fallback argument (${args.attachments[0].fallback === "  " ? "empty" : args.attachments[0].fallback})`, function () {
+        it(`should send both text and fallback-argument-specific warning to logs when client executes ${method} without text argument(${args.text === "" ? "empty" : args.text}) nor without attachment-level fallback argument (${args.attachments[0].fallback === "  " ? "empty" : args.attachments[0].fallback})`, function () {
           const logger = {
             debug: sinon.spy(),
             info: sinon.spy(),
@@ -290,7 +316,7 @@ describe('WebClient', function () {
           const warnClient = new WebClient(token, { logLevel: LogLevel.WARN, logger });
           return warnClient.apiCall(method, args)
             .then(() => {
-              assert.equal(logger.warn.callCount, 4)
+              assert.equal(logger.warn.callCount, 5)
             });
         });
       });
--- packages/web-api/src/WebClient.ts
+++ packages/web-api/src/WebClient.ts
@@ -699,8 +699,10 @@ function warnIfFallbackIsMissing(method: string, logger: Logger, options?: WebAP
   const targetMethods = ['chat.postEphemeral', 'chat.postMessage', 'chat.scheduleMessage', 'chat.update'];
   const isTargetMethod = targetMethods.includes(method);
 
+  const hasAttachments = (args: WebAPICallOptions) => Array.isArray(args.attachments) && args.attachments.length;
+
   const missingAttachmentFallbackDetected = (args: WebAPICallOptions) => Array.isArray(args.attachments) &&
-    args.attachments.length && args.attachments.some((attachment) => !attachment.fallback || attachment.fallback.trim() === '');
+    args.attachments.some((attachment) => !attachment.fallback || attachment.fallback.trim() === '');
 
   const isEmptyText = (args: WebAPICallOptions) => args.text === undefined || args.text === null || args.text === '';
 
@@ -713,12 +715,13 @@ function warnIfFallbackIsMissing(method: string, logger: Logger, options?: WebAP
     'To avoid this warning, it is recommended to always provide a top-level `text` argument when posting a message. ' +
     'Alternatively, you can provide an attachment-level `fallback` argument, though this is now considered a legacy field (see https://api.slack.com/reference/messaging/attachments#legacy_fields for more details).';
   if (isTargetMethod && typeof options === 'object') {
-    if (isEmptyText(options)) {
-      if (missingAttachmentFallbackDetected(options)) {
-        logger.warn(buildMissingFallbackWarning());
-      } else {
+    if (hasAttachments(options)) {
+      if (missingAttachmentFallbackDetected(options) && isEmptyText(options)) {
         logger.warn(buildMissingTextWarning());
+        logger.warn(buildMissingFallbackWarning());
       }
+    } else if (isEmptyText(options)) {
+      logger.warn(buildMissingTextWarning());
     }
   }
 }
