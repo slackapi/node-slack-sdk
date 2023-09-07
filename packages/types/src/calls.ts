d554e3ee3b52b0121d79ec017a23a4a6b6b27ecc
Filip Maj <fmaj@slack-corp.com>
2023-09-07 15:08:00 -0400

    Adding jsdocs to Calls types. `avatar_url` for external Call user is optional

--- packages/types/src/calls.ts
+++ packages/types/src/calls.ts
@@ -1,11 +1,31 @@
+// These types represent users in Slack Calls, which is an API for showing 3rd party calls within the Slack client.
+// More information on the API guide for Calls: https://api.slack.com/apis/calls
+// and on User objects for use with Calls: https://api.slack.com/apis/calls#users
+
+/**
+ * @description For use in representing {@link https://api.slack.com/apis/calls#users users in a Slack Call}.
+ */
 export type CallUser = CallUserSlack | CallUserExternal;
 
 export interface CallUserSlack {
+  /**
+   * @description The Slack encoded user ID, e.g. U1234ABCD. Set this if you have it or know it, otherwise, set
+   * `external_id` and `display_name`.
+   */
   slack_id: string;
 }
 
 export interface CallUserExternal {
+  /**
+   * @description A unique ID created by your app to represent your users.
+   */
   external_id: string;
+  /**
+   * @description Name of the user to be displayed in the Call block in a message.
+   */
   display_name: string;
-  avatar_url: string;
+  /**
+   * @description URL to an avatar image of the user.
+   */
+  avatar_url?: string;
 }
