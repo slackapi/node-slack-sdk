1374422c542637a0628acdc83a542f4c8cf09895
Kazuhiro Sera <seratch@gmail.com>
2023-10-05 07:57:58 +0900

    Add deprecation comment

--- packages/web-api/src/methods.ts
+++ packages/web-api/src/methods.ts
@@ -1884,7 +1884,10 @@ interface FileUpload {
 
 export interface FilesUploadV2Arguments extends FileUploadV2, WebAPICallOptions, TokenOverridable {
   file_uploads?: Omit<FileUploadV2, 'channel_id' | 'channels' | 'initial_comment' | 'thread_ts'>[];
-  request_file_info?: boolean; // since v7, this flag is no longer used
+  /**
+   * @deprecated Since v7, this flag is no longer used. You can safely remove it from your code.
+   */
+  request_file_info?: boolean;
 }
 
 export type FileUploadV2 = FileUpload & {
