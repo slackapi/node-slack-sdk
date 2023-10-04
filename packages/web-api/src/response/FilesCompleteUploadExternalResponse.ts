7857f26a40113fb2f2735e2975a1cf1ae291ce11
Kazuhiro Sera <seratch@gmail.com>
2023-10-04 14:35:18 +0900

    Update files.upload.v2 internals due to server-side improvements

--- packages/web-api/src/response/FilesCompleteUploadExternalResponse.ts
+++ packages/web-api/src/response/FilesCompleteUploadExternalResponse.ts
@@ -18,6 +18,57 @@ export type FilesCompleteUploadExternalResponse = WebAPICallResult & {
 };
 
 export interface File {
-  id?:    string;
-  title?: string;
+  channels?:             string[];
+  comments_count?:       number;
+  created?:              number;
+  display_as_bot?:       boolean;
+  edit_link?:            string;
+  editable?:             boolean;
+  external_type?:        string;
+  file_access?:          string;
+  filetype?:             string;
+  groups?:               string[];
+  has_more_shares?:      boolean;
+  has_rich_preview?:     boolean;
+  id?:                   string;
+  ims?:                  string[];
+  is_external?:          boolean;
+  is_public?:            boolean;
+  is_starred?:           boolean;
+  lines?:                number;
+  lines_more?:           number;
+  media_display_type?:   string;
+  mimetype?:             string;
+  mode?:                 string;
+  name?:                 string;
+  permalink?:            string;
+  permalink_public?:     string;
+  pretty_type?:          string;
+  preview?:              string;
+  preview_highlight?:    string;
+  preview_is_truncated?: boolean;
+  public_url_shared?:    boolean;
+  shares?:               Shares;
+  size?:                 number;
+  timestamp?:            number;
+  title?:                string;
+  url_private?:          string;
+  url_private_download?: string;
+  user?:                 string;
+  user_team?:            string;
+  username?:             string;
+}
+
+export interface Shares {
+  public?: { [key: string]: Public[] };
+}
+
+export interface Public {
+  channel_name?:      string;
+  reply_count?:       number;
+  reply_users?:       string[];
+  reply_users_count?: number;
+  share_user_id?:     string;
+  team_id?:           string;
+  ts?:                string;
 }
