0a2692da01c502dfd5604da6f4e92fe98289e5ce
Filip Maj <fmaj@slack-corp.com>
2023-10-18 17:00:05 -0400

    users methods

--- packages/web-api/src/methods.ts
+++ packages/web-api/src/methods.ts
@@ -2393,45 +2393,54 @@ export interface UsergroupsUsersUpdateArguments extends TokenOverridable, Option
 /*
  * `users.*`
  */
-export interface UsersConversationsArguments extends TokenOverridable, CursorPaginationEnabled {
+// https://api.slack.com/methods/users.conversations
+export interface UsersConversationsArguments extends TokenOverridable, CursorPaginationEnabled, OptionalTeamAssignable {
   exclude_archived?: boolean;
   types?: string; // comma-separated list of conversation types
   user?: string;
-  team_id?: string;
 }
 cursorPaginationEnabledMethods.add('users.conversations');
+// https://api.slack.com/methods/users.deletePhoto
 export interface UsersDeletePhotoArguments extends TokenOverridable { }
+// https://api.slack.com/methods/users.getPresence
 export interface UsersGetPresenceArguments extends TokenOverridable {
-  user: string;
+  user?: string;
 }
+// https://api.slack.com/methods/users.identity
 export interface UsersIdentityArguments extends TokenOverridable { }
+// https://api.slack.com/methods/users.info
 export interface UsersInfoArguments extends TokenOverridable, LocaleAware {
   user: string;
 }
-export interface UsersListArguments extends TokenOverridable, CursorPaginationEnabled, LocaleAware {
-  presence?: boolean; // deprecated, defaults to false
-  team_id?: string;
-}
+// https://api.slack.com/methods/users.list
+export interface UsersListArguments extends TokenOverridable, CursorPaginationEnabled,
+  LocaleAware, OptionalTeamAssignable { }
 cursorPaginationEnabledMethods.add('users.list');
+// https://api.slack.com/methods/users.lookupByEmail
 export interface UsersLookupByEmailArguments extends TokenOverridable {
   email: string;
 }
+// https://api.slack.com/methods/users.setPhoto
 export interface UsersSetPhotoArguments extends TokenOverridable {
   image: Buffer | Stream;
   crop_w?: number;
   crop_x?: number;
   crop_y?: number;
 }
+// https://api.slack.com/methods/users.setPresence
 export interface UsersSetPresenceArguments extends TokenOverridable {
   presence: 'auto' | 'away';
 }
+// https://api.slack.com/methods/users.profile.get
 export interface UsersProfileGetArguments extends TokenOverridable {
   include_labels?: boolean;
   user?: string;
 }
+// TODO: breaking change: either profile or name/value pair must be provided
+// https://api.slack.com/methods/users.profile.set
 export interface UsersProfileSetArguments extends TokenOverridable {
   profile?: string; // url-encoded json
-  user?: string;
+  user?: string; // must be an admin user and must be on a paid plan
   name?: string; // usable if `profile` is not passed
   value?: string; // usable if `profile` is not passed
 }
