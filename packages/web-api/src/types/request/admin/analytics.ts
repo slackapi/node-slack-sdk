94c7c57143b1d47d921ff25708e13cec7fc2d813
Filip Maj <fmaj@slack-corp.com>
2023-11-28 10:07:27 -0500

    Refactor arguments and type tests for admin.analytics.getFile API.

--- packages/web-api/src/types/request/admin/analytics.ts
+++ packages/web-api/src/types/request/admin/analytics.ts
@@ -1,11 +1,44 @@
 import type { TokenOverridable } from '../common';
 
-// TODO: breaking changes - potential type improvements:
-// - date is required _except_ if metadata_only=true. metadata=true, then we are to omit it (according to docs)
-// - metadata_only=true only works with type=public_channel
-// https://api.slack.com/methods/admin.analytics.getFile
-export interface AdminAnalyticsGetFileArguments extends TokenOverridable {
-  type: 'public_channel' | 'member';
-  date?: string;
-  metadata_only?: boolean;
+interface Date {
+  /**
+   * @description Date to retrieve the analytics data for, expressed as `YYYY-MM-DD` in UTC.
+   * Required unless `metadata_only` is set to `true`.
+   */
+  date: string;
 }
+interface MemberArgs extends Date {
+  /**
+   * @description The type of analytics to retrieve. The options are currently limited to
+   * `member` (for grid member analytics) and `public_channel` (for public channel analytics).
+   */
+  type: 'member';
+}
+interface TypeChannel {
+  /**
+   * @description The type of analytics to retrieve. The options are currently limited to
+   * `member` (for grid member analytics) and `public_channel` (for public channel analytics).
+   */
+  type: 'public_channel';
+}
+interface PublicChannelWithDate extends TypeChannel, Date {
+  /**
+   * @description Retrieve metadata for the `type` of analytics indicated. Can be used only with `type` set to
+   * `public_channel` analytics. Omit the date parameter when using this argument. Defaults to `false`.
+   * @see {@link https://api.slack.com/methods/admin.analytics.getFile#metadata_only Public channel metadata}.
+   */
+  metadata_only?: false;
+}
+interface PublicChannelWithMetadata extends TypeChannel {
+  /**
+   * @description Retrieve metadata for the `type` of analytics indicated. Can be used only with `type` set to
+   * `public_channel` analytics. Omit the date parameter when using this argument. Defaults to `false`.
+   * @see {@link https://api.slack.com/methods/admin.analytics.getFile#metadata_only Public channel metadata}.
+   */
+  metadata_only: true;
+}
+// when type=public_channel, either date must be provided, or metadata_only must be set to true, but not both
+type PublicChannelArgs = PublicChannelWithDate | PublicChannelWithMetadata;
+
+// https://api.slack.com/methods/admin.analytics.getFile
+export type AdminAnalyticsGetFileArguments = TokenOverridable & (MemberArgs | PublicChannelArgs);
