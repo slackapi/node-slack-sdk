22efb5a0ef3f13cd317587be7407d58a2aca181e
Filip Maj <fmaj@slack-corp.com>
2023-12-07 14:49:12 -0500

    fix bad reference

--- packages/web-api/src/types/response/FunctionsCompleteErrorResponse.ts
+++ packages/web-api/src/types/response/FunctionsCompleteErrorResponse.ts
@@ -8,7 +8,7 @@
 //                                                                                     //
 /////////////////////////////////////////////////////////////////////////////////////////
 
-import { WebAPICallResult } from '../WebClient';
+import { WebAPICallResult } from '../../WebClient';
 export type FunctionsCompleteErrorResponse = WebAPICallResult & {
   error?:    string;
   needed?:   string;
--- packages/web-api/src/types/response/FunctionsCompleteSuccessResponse.ts
+++ packages/web-api/src/types/response/FunctionsCompleteSuccessResponse.ts
@@ -8,7 +8,7 @@
 //                                                                                     //
 /////////////////////////////////////////////////////////////////////////////////////////
 
-import { WebAPICallResult } from '../WebClient';
+import { WebAPICallResult } from '../../WebClient';
 export type FunctionsCompleteSuccessResponse = WebAPICallResult & {
   error?:    string;
   needed?:   string;
