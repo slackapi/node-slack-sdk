920c3fb7f53f7201d75f9c026e56f9fae1e1be80
Fil Maj <fmaj@slack-corp.com>
2023-12-14 08:10:25 -0500

    Update packages/web-api/src/types/request/bots.ts
    Co-authored-by: Kazuhiro Sera <seratch@gmail.com>

--- packages/web-api/src/types/request/bots.ts
+++ packages/web-api/src/types/request/bots.ts
@@ -2,6 +2,6 @@ import type { OptionalTeamAssignable, TokenOverridable } from './common';
 
 // https://api.slack.com/methods/bots.info
 export interface BotsInfoArguments extends TokenOverridable, OptionalTeamAssignable {
-  /** @description Bot user ID to retrieve information about. */
+  /** @description Bot ID, which starts with 'B', to retrieve information about. */
   bot?: string;
 }
