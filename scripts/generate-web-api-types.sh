27535f13a80755abafaace54e06644e33946c766
Fil Maj <fmaj@slack-corp.com>
2024-06-19 08:35:03 -0400

    chore: tweak response types generation script (#1825)

--- scripts/generate-web-api-types.sh
+++ scripts/generate-web-api-types.sh
@@ -3,18 +3,20 @@
 script_dir=`dirname $0`
 tmp_dir=${script_dir}/../tmp
 
-rm -rf ${tmp_dir}
-mkdir ${tmp_dir}
-cd ${tmp_dir}
+mkdir -p ${tmp_dir}
+pushd ${tmp_dir}
 
 # Use the latest revision of the java-slack-sdk git repo
-git clone https://github.com/slackapi/java-slack-sdk.git
-
-cd -
-cd ${script_dir}
+test -d java-slack-sdk || git clone https://github.com/slackapi/java-slack-sdk.git
+pushd java-slack-sdk
+git pull origin main
+popd
+popd
+pushd ${script_dir}
 
 # Download quicktype for generating code
 npm i
 
 # This Ruby script runs quicktype internally and do some additional modification to the generated files
 ruby ./code_generator.rb
+popd
