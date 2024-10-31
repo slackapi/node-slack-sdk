2031b63afc0acf70ea7d57138a113c07fbd90b63
Luke Russell <luke.russell@slack-corp.com>
2024-10-31 09:32:23 -0700

    docs: salesforce compliance adjustments

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,10 +1,6 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
+const footer = require('./footerConfig');
+const navbar = require('./navbarConfig');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -41,9 +37,6 @@ const config = {
         blog: false,
         theme: {
           customCss: './src/css/custom.css',
-        },
-        gtag: {
-          trackingID: 'G-9H1YZW28BG',
         },
       }),
     ],
@@ -173,98 +166,8 @@ const config = {
           autoCollapseCategories: true,
         },
       },
-      navbar: {
-        title: "Slack Developer Tools",
-        logo: {
-          alt: 'Slack logo',
-          src: 'img/slack-logo.svg',
-          href: 'https://tools.slack.dev',
-          target : '_self'
-        },
-        items: [
-          {
-            type: 'dropdown',
-            label: 'Bolt',
-            position: 'left',
-            items: [
-              {
-                label: 'Java',
-                to: 'https://tools.slack.dev/java-slack-sdk/guides/bolt-basics',
-                target: '_self',
-              },
-              {
-                label: 'JavaScript',
-                to: 'https://tools.slack.dev/bolt-js',
-                target: '_self',
-              },
-              {
-                label: 'Python',
-                to: 'https://tools.slack.dev/bolt-python',
-                target: '_self',
-              },
-            ]
-          },
-          {
-            type: 'dropdown',
-            label: 'SDKs',
-            position: 'left',
-            items: [
-              {
-                label: 'Java Slack SDK',
-                to: 'https://tools.slack.dev/java-slack-sdk/',
-                target: '_self',
-              },
-              {
-                label: 'Node Slack SDK',
-                to: 'https://tools.slack.dev/node-slack-sdk/',
-                target: '_self',
-              },
-              {
-                label: 'Python Slack SDK',
-                to: 'https://tools.slack.dev/python-slack-sdk/',
-                target: '_self',
-              },
-              {
-                label: 'Deno Slack SDK',
-                to: 'https://api.slack.com/automation/quickstart',
-                target: '_self',
-              },
-            ]
-          },
-          {
-            type: 'dropdown',
-            label: 'Community',
-            position: 'left',
-            items: [
-              {
-                label: 'Community tools',
-                to: 'https://tools.slack.dev/community-tools',
-                target: '_self',
-              },
-              {
-                label: 'Slack Community',
-                to: 'https://slackcommunity.com/',
-                target: '_self',
-              },
-            ]
-          },
-          {
-            to: 'https://api.slack.com/docs',
-            label: 'API Docs',
-            target: '_self',
-          },
-          {
-            'aria-label': 'GitHub Repository',
-            'className': 'navbar-github-link',
-            'href': 'https://github.com/slackapi/node-slack-sdk',
-            'position': 'right',
-            target: '_self',
-          },
-        ],
-      },
-      footer: {
-        copyright: `<p> Made with ♡ by Slack and pals like you <p>`,
-      },
+      navbar,
+      footer,
       prism: {
         // switch to alucard when available in prism?
         theme: prismThemes.github, 
