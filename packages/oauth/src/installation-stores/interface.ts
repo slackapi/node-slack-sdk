e61dd808a720678d25fdebd1f79b803804c6ae91
Ashley Huynh <ashley.huynh@slack-corp.com>
2024-11-21 17:05:51 -0500

    Add initial methods for installation store (delete bot, delete all)

--- packages/oauth/src/installation-stores/interface.ts
+++ packages/oauth/src/installation-stores/interface.ts
@@ -13,5 +13,27 @@ export interface InstallationStore {
     logger?: Logger,
   ) => Promise<Installation<'v1' | 'v2', boolean>>;
 
+  /**
+   * Deletes an installation that matches the given IDs
+   * @param query 
+   * @param logger 
+   * @returns Promise<void>
+   */
   deleteInstallation?: (query: InstallationQuery<boolean>, logger?: Logger) => Promise<void>;
+
+  /**
+   * Deletes a bot scope installation per workspace / org
+   * @param query 
+   * @param logger 
+   * @returns Promise<void>
+   */
+  deleteBot?: (query: InstallationQuery<boolean>, logger?: Logger) => Promise<void>;
+
+  /**
+   * Deletes all installation data for the given workspace / org
+   * @param query 
+   * @param logger 
+   * @returns Promise<void>
+   */
+  deleteAll?: (query: InstallationQuery<boolean>, logger?: Logger) => Promise<void>;
 }
