233ca6d8954f7d345bdec215c5b45c9d3480da8b
William Bergamin <wbergamin@salesforce.com>
2025-05-16 13:42:54 +0000

    fix: add trailing slash to slackApiUrl if none is provided (#2243)

--- packages/web-api/src/WebClient.ts
+++ packages/web-api/src/WebClient.ts
@@ -286,6 +286,9 @@ export class WebClient extends Methods {
 
     this.token = token;
     this.slackApiUrl = slackApiUrl;
+    if (!this.slackApiUrl.endsWith('/')) {
+      this.slackApiUrl += '/';
+    }
 
     this.retryConfig = retryConfig;
     this.requestQueue = new pQueue({ concurrency: maxRequestConcurrency });
@@ -311,7 +314,7 @@ export class WebClient extends Methods {
     this.axios = axios.create({
       adapter: adapter ? (config: InternalAxiosRequestConfig) => adapter({ ...config, adapter: undefined }) : undefined,
       timeout,
-      baseURL: slackApiUrl,
+      baseURL: this.slackApiUrl,
       headers: isElectron() ? headers : { 'User-Agent': getUserAgent(), ...headers },
       httpAgent: agent,
       httpsAgent: agent,
