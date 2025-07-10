e2751543a5b700f9d302d6bf586258e644e3ab59
Haley Elmendorf <helmendorf@slack-corp.com>
2025-07-10 09:55:04 -0500

    deleteing a file I did not mean to commit

--- docs/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
