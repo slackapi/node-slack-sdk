61b8033b12c7e751dc14f5bf7c2b65fa4a221310
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-09-20 12:28:54 -0700

    fix(types): jsdoc link to a single icon button

--- packages/types/src/block-kit/blocks.ts
+++ packages/types/src/block-kit/blocks.ts
@@ -134,7 +134,7 @@ export interface ContextActionsBlock extends Block {
    */
   type: 'context_actions';
   /**
-   * @description An array of {@link FeedbackButtons} or {@link IconButtons} objects.
+   * @description An array of {@link FeedbackButtons} or {@link IconButton} objects.
    */
   elements: ContextActionsBlockElement[];
 }
