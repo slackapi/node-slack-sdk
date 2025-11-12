1b533e40b6ad4c3623a61c735a8798a473b5c9d9
Maria Alejandra <104795114+srtaalej@users.noreply.github.com>
2025-11-12 13:44:49 -0500

    Update packages/web-api/src/types/request/slacklists.ts
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>

--- packages/web-api/src/types/request/slacklists.ts
+++ packages/web-api/src/types/request/slacklists.ts
@@ -6,7 +6,7 @@ import type { TokenOverridable } from './common';
 export interface SlackListsCreateArguments extends TokenOverridable {
   name: string;
   description_blocks?: Array<RichTextBlock>;
-  schema?: Array<Record<string, Block | KnownBlock>>;
+  schema?: Array<Record<string, unknown>>;
   copy_from_list_id?: string;
   include_copied_list_records?: boolean;
   todo_mode?: boolean;
